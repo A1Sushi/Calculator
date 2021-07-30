b1c5d4ee1075a23dfec3cbd655c2dd7d73095f0f
Tommy Tran <tommytran99@outlook.com>
2021-07-30 16:50:08 -0400

    Create functions that populate display when numbers are clicked

--- script.js
+++ script.js
@@ -29,31 +29,43 @@ function operate (operator, x, y) {
 }
 // console.log(operate(add, 2, 2)); 
 
-// Step 4) Function to display # buttons clicked 
-    // if text is a num convert it to one *
-        // else, return just the text value
-        // Will create sepearte functions for those buttons later
-    // should change text.content or add to it 
-        // 
-
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    // add 'click' for each single button 
-    button.addEventListener('click', (e) => {
-        // Call function here for when button is clicked
-        // return the text value as a number 
-        let displayValue = Number(e.target.textContent)
-        const screen = document.querySelector('.cell1')
-        screen.textContent = displayValue
-        console.log(displayValue)
-    })
-})
-
-/* Plan 
-1. Should change text.content on cell1 / screen, 
-    
+/* Function to display # buttons clicked 
+    - Store value clicked in variable *
+    - if values match are between 0-9 *
+        - Convert the value to a number *
+        - display it on the screen *
+    - Will create sepearte functions for those buttons late
 */
+
+function display() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        // add 'click' for each single button 
+        button.addEventListener('click', (e) => {
+            // Call function here for when button is clicked
+            // return the text value as a number 
+            // let value = Number(e.target.textContent)
+            let value = e.target.textContent
+            isNum(value)
+        })
+    })
+}
+display()
+
+function isNum(value) {
+    if (value >= 0 && value <= 10) {
+        const screen = document.querySelector('.cell1')
+        // Add display value based on what was clicked 
+        screen.textContent = value
+        value = Number(value)
+        console.log(value)
+    // will be to call other functions 
+    } else {
+        // console.log("naw")
+    }
+}
+
+
 
 
 
