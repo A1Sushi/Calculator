addf0c6934a6edeeb0289feb741cb2b139299a8d
Tommy Tran <tommytran99@outlook.com>
2021-08-10 14:52:15 -0400

    Add function to allow chaining operations

--- script.js
+++ script.js
@@ -75,26 +75,63 @@ function display(value) {
         - Display answer on screen *
 */
 
+/* Step 6: Chaining operations together @@@
+-> Chaining another operator should store answer of two numbers as 1st number for new calculation 
+1. When operator is pressed:
+    - Check if displayOne == undefined, if yes; 
+        -> Save number to it 
+    - if not; 
+        -> save number to displayTwo
+        -> Perform operation with two values 
+        -> save answer to displayOne 
+*/
+
 // Clear Function
 function clearScreen() {
     screen.textContent = ""
     buttonsClicked = ""
 }
 
+function clearValues() {
+    displayOne = 0
+    displayTwo = 0
+}
+
+// Attach clear button to function
+const clear = document.querySelector('.cell2');
+clear.addEventListener('click', () => {
+    clearScreen();
+    clearValues()
+})
+
 // "=" Key
 let displayTwo
-let answer
 function equal() {
     displayTwo = Number(screen.textContent)
     console.log(displayTwo)
-    clearScreen();
-    answer = operate(operator, displayOne, displayTwo)
-    screen.textContent = answer
+    // clearScreen();
+    // answer = operate(operator, displayOne, displayTwo)
+    // screen.textContent = answer
+    result()
 }
 
+// Operation for getting answer @@
+function result() {
+    // Recently changed from answer 
+    displayOne = operate(operator, displayOne, displayTwo)
+    // console.log(displayOne)
+    screen.textContent = displayOne
+}
+
+// Attaching = to = button 
 const equalButton = document.querySelector('.cell20');
 equalButton.addEventListener('click', () => {
-    equal()
+    // Fix issue with = button (part 3 step 6)
+    if (displayOne == undefined || displayTwo == undefined) {
+        return 
+    } else {
+     equal()
+    }
 })
 
 // for storing operator selected
@@ -103,39 +140,57 @@ let operator
 const divison = document.querySelector('.cell5');
 divison.addEventListener('click', () => {
     // Get the current values on screen and store in a variable
-    displayOne = Number(screen.textContent)
+    resultCondition()
     console.log(displayOne)
     // Save divide operator
     operator = divide;
+    // if...else statement to clear screen or add result
     clearScreen();
+    screen.textContent = displayOne
 })
 
 // EL for muliplication 
 const multiplication = document.querySelector('.cell9');
 multiplication.addEventListener('click', () => {
     // Get the current values on screen and store in a variable
-    displayOne = Number(screen.textContent)
+    resultCondition()
     console.log(displayOne)
     operator = multiply;
     clearScreen();
+    screen.textContent = displayOne
 })
 
 // EL for subtraction @
 const subtraction = document.querySelector('.cell13');
 subtraction.addEventListener('click', () => {
     // Get the current values on screen and store in a variable
-    displayOne = Number(screen.textContent)
+    resultCondition()
     console.log(displayOne)
     operator = subtract;
     clearScreen();
+    screen.textContent = displayOne
 })
 
 // EL for addition
 const addition = document.querySelector('.cell17');
 addition.addEventListener('click', () => {
     // Get the current values on screen and store in a variable
-    displayOne = Number(screen.textContent)
+    resultCondition()
     console.log(displayOne)
+    // Save operator
     operator = add;
     clearScreen();
+    screen.textContent = displayOne
 })
+
+function resultCondition() {
+    if (displayOne == undefined) {
+        displayOne = Number(screen.textContent)
+    } else {
+        displayTwo = Number(screen.textContent)
+        result()
+    }
+}
+
+// Question
+    // Hey everyone, I'm currently working on step 6 part 1 of project calculator where I have to chain the operations together. So far my code works as long as you press the equals button after selecting 2 numbers, but I'm trying to figure out how to get the answer to display: If you enter a number then an operator and another number that calculation should be displayed if your next input is an operator. The result of the calculation should be used as the first number in your new calculation. I have a feeling this step is very similar to the fibonacci exercise from fundamentals part 5, but I'm trying to figure out how I can apply that to this scenario. 
